924a8294a772288cb940cd49d28c5f75cf4d89fb
SEONKYU\difke <somedaycode@gmail.com>
2021-04-25 19:49:32 +0900

    Add. socket.io를 이용해 두명이상이 동시에 글을 작성할 수 있게 하였다

--- client/src/components/TextEditor.js
+++ client/src/components/TextEditor.js
@@ -1,6 +1,7 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
+import { io } from 'socket.io-client';
 
 const TOOLBAR_OPTIONS = [
   ['bold', 'italic', 'underline', 'strike'],
@@ -16,13 +17,60 @@ const TOOLBAR_OPTIONS = [
 ];
 
 const TextEditor = () => {
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
+
+  useEffect(() => {
+    const s = io('http://localhost:3001');
+    setSocket(s);
+
+    return () => {
+      s.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket === undefined || quill === undefined) return;
+
+    const handler = (delta) => {
+      quill.updateContents(delta);
+    };
+
+    socket.on('receive-changes', handler);
+
+    return () => {
+      socket.off('receive-changes', handler);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (socket === undefined || quill === undefined) return;
+
+    const handler = (delta, oldDelta, source) => {
+      if (source !== 'user') return;
+      socket.emit('send-changes', delta);
+    };
+
+    quill.on('text-change', handler);
+
+    return () => {
+      quill.off('text-change', handler);
+    };
+  }, [socket, quill]);
+
   const wrapperRef = useCallback((wrapper) => {
     if (wrapper === null) return;
 
     wrapper.innerHTML = '';
     const editor = document.createElement('div');
     wrapper.append(editor);
-    new Quill(editor, { theme: 'snow', modules: { toolbar: TOOLBAR_OPTIONS } });
+
+    const q = new Quill(editor, {
+      theme: 'snow',
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    });
+
+    setQuill(q);
   }, []);
 
   return <div className="container" ref={wrapperRef}></div>;
