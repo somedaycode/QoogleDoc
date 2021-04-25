450bb9300f2f4e2e0c88cfb65f50bcca1d42b5d6
SEONKYU\difke <somedaycode@gmail.com>
2021-04-25 20:04:44 +0900

    Add. react-router-dom 설정 및 uuid 적용

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import TextEditor from 'components/TextEditor.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
 const App = () => {
-  return <TextEditor />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to={`/documents/${uuidv4()}`} />
+        </Route>
+        <Route path="/documents/:id">
+          <TextEditor />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
